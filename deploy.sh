5febd0c3af35346ee994bc50f1dcd1dc83c47221
Pison Golda <pison.gm.dev@gmail.com>
2026-03-30 23:30:49 +0700

    fit deployment environment(4)

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ git pull origin main
 echo "📦 Membangun ulang container..."
 docker compose up -d --build
 
-# Jalankan Migrasi
+# Jalankan Migrasi dengan Path Absolut yang Benar
 echo "⏳ Mengecek dan Menjalankan Migrasi Database..."
-docker run --rm --network host -v $(pwd)/paimei-api/db/migrations:/migrations migrate/migrate -path=/migrations/ -database "postgres://user_paimei:dbpaimei123@localhost:5435/paimei_db?sslm>
+docker run --rm --network host -v /home/user_pison/projects/paimei/paimei-api/internal/core/db/migrations:/migrations migrate/migrate -path=/migrations/ -database "postgres://user_paimei:dbpaimei123@localhost:5435/paimei_db?sslmode=disable" up
 
 # Bersihkan image lama
 docker image prune -f
